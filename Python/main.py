2967c0e5133ed1c29cadf75cd61e3b4c9dec0ed7
James Miranda <jameswpm@gmail.com>
2023-09-21 15:46:39 +0200

    Working code to get traces based on class name

--- Python/main.py
+++ Python/main.py
@@ -7,6 +7,7 @@ from graph.model2graph import Model2Graph
 
 #Internal
 from modeling.metamodels import Metamodels
+from modeling.traces import Traces
 
 RESOURCES_PATH = osp.join(Path(__file__).parent, '..', 'resources')
 
@@ -21,12 +22,12 @@ xmi_path_left = osp.join(RESOURCES_PATH, 'models', 'yakindu.xmi')
 m_resource_left = resource_set.get_resource(URI(xmi_path_left))
 m_resource_left.use_uuid = True
 # #save resource with UUIDs at temporary directory
-# m_resource_left.save(output=URI(osp.join(RESOURCES_PATH, 'models', 'temp', 'yakindu.xmi')))
+m_resource_left.save(output=URI(osp.join(RESOURCES_PATH, 'models', 'temp', 'yakindu.xmi')))
 
 xmi_path_right = osp.join(RESOURCES_PATH, 'models', 'statecharts-emftvm.xmi')
 m_resource_right = resource_set.get_resource(URI(xmi_path_right))
-# m_resource_right.use_uuid = True
-# m_resource_right.save(output=URI(osp.join(RESOURCES_PATH, 'models', 'temp', 'statecharts-emftvm.xmi')))
+m_resource_right.use_uuid = True
+m_resource_right.save(output=URI(osp.join(RESOURCES_PATH, 'models', 'temp', 'statecharts-emftvm.xmi')))
 
 #TODO: How to define the labels?
 model_to_graph_left = Model2Graph(label="Y")
@@ -42,9 +43,15 @@ merged_graph = model_to_graph_left.get_hetero_graph().update(model_to_graph_righ
 model_to_graph_traces = Model2Graph()
 xmi_path_traces = osp.join(RESOURCES_PATH, 'models', 'yakindu_stratecharts_traces.xmi')
 m_resource_traces = resource_set.get_resource(URI(xmi_path_traces))
-model_to_graph_traces.get_graph_from_model(m_resource_traces)
+m_resource_traces.use_uuid = True
+m_resource_traces.save(output=URI(osp.join(RESOURCES_PATH, 'models', 'temp', 'yakindu_stratecharts_traces.xmi')))
 
-print(model_to_graph_traces.get_hetero_graph())
+# Use the traces class to get a mapping of the traces by class
+traces = Traces(m_resource_traces)
+mapping = traces.get_mapping_traces('State')
+
+
+print(mapping)
 
 
 
--- Python/modeling/traces.py
+++ Python/modeling/traces.py
@@ -1,3 +1,28 @@
 
 
 class Traces():
+
+    def __init__(self, model_resource):
+        """_summary_
+        """
+
+        self.model_resource = model_resource
+        self.mapping = {}
+    
+    def get_mapping_traces(self, src_class_name):
+        for root in self.model_resource.contents:
+            for element in root.eAllContents():
+                class_name = element.eClass.name
+                if class_name == 'TraceLink':
+                    #TODO: It works only for 1to1 relation. Fix it to be more generic                    
+                    source_element = element.sourceElements[0]
+                    object_element = source_element.object
+                    object_element_class = object_element.eClass.name
+                    if object_element_class == src_class_name:
+                        #get the target of the relation
+                        target_element = element.targetElements[0].object
+                        self.mapping[object_element._internal_id] = target_element._internal_id
+        
+        return self.mapping
+
+                
