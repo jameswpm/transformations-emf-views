fdc2ca365c4123bbbbaf937ee96a98b2d6df1373
James Miranda <jameswpm@gmail.com>
2023-09-22 15:08:50 +0200

    Include a GNN architecture for tests

--- Python/main.py
+++ Python/main.py
@@ -57,6 +57,7 @@ left_nodes_mapping = model_to_graph_left.get_mapping_nodes()
 right_nodes_mapping = model_to_graph_right.get_mapping_nodes()
 for src_uuid, target_uuid in mapping.items():
     edges.append([left_nodes_mapping[src_uuid], right_nodes_mapping[target_uuid]])
+
 merged_graph['state', 'to', 'state'].edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
 
 print(merged_graph.edge_types)
@@ -73,6 +74,8 @@ from sklearn.metrics import precision_score
 import warnings
 warnings.filterwarnings("ignore")
 from torch_geometric.transforms import RandomLinkSplit
+from torch_geometric.data import HeteroData
+import torch_geometric.transforms as T
 import time
 
 
@@ -98,6 +101,7 @@ else:
   dev = "cpu"  
 device = torch.device(dev)
 
+merged_graph = T.ToUndirected()(merged_graph)
 
 transform = RandomLinkSplit(
    is_undirected=False,
@@ -121,6 +125,129 @@ print(len(merged_graph['state', 'to', 'state'].edge_index[0]) )  # all existing 
 print(merged_graph.edge_types)
 
 
+class Encoder(nn.Module):
+  """
+  basic gnn with two layers to get representations for every node  
+  """
+  def __init__(self, node_dim=64):
+    super(Encoder,self).__init__()   
+    self.gconv1  = gnn.SAGEConv((-1,-1),  node_dim)
+    self.gconv2  = gnn.SAGEConv((-1,-1), node_dim)
+
+  def forward(self, x, edge_index):
+    h = self.gconv1(x, edge_index).relu()
+    h = self.gconv2(h, edge_index)
+    return h
+
+EPS = 1e-15
+
+class EdgeDecoder(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.lin1 = nn.Linear(2 * hidden_channels, hidden_channels)
+        self.lin2 = nn.Linear(hidden_channels, 1)
+
+    def forward(self, z_dict, edge_label_index):
+        z = torch.cat([z_dict['state'][edge_label_index[0]], z_dict['state'][edge_label_index[1]]], dim=-1)
+
+        z = self.lin1(z).relu()
+        z = self.lin2(z).sigmoid()
+        return z.view(-1)
 
 
+class MyGAE(nn.Module):
 
+  def __init__(self, encoder,decoder, data:HeteroData, node_types:List[Tuple], node_dim:int):
+    
+    super(MyGAE,self).__init__()
+    self.node_types = {}
+    for n in node_types:
+      self.node_types[n[0]]  = nn.Embedding(n[1], node_dim) # this part is redundant(we can use embeddings from torch geometric directly)
+      self.node_types[n[0]].to(device)
+    self.hetero_encoder = to_hetero(encoder, data.metadata())
+    self.decoder = decoder
+
+  def encode(self, nodes, edges):
+    xVectors= {}
+    for i in self.node_types.keys():
+      xVectors[i] = self.node_types[i](nodes[i])
+    return self.hetero_encoder(xVectors, edges)
+    
+  def decode(self,h,edge_label_index):
+    
+    return self.decoder(h,edge_label_index)
+
+  def recon_loss(self, z, pos_edge_index, neg_edge_index):
+        r"""Given latent variables :obj:`z`, computes the binary cross
+        entropy loss for positive edges :obj:`pos_edge_index` and negative
+        sampled edges.
+        """
+
+        pos_loss = -torch.log(
+            self.decode(z, pos_edge_index) + EPS).mean()
+
+        # Do not include self-loops in negative samples
+        # pos_edge_index, _ = remove_self_loops(pos_edge_index)
+        # pos_edge_index, _ = add_self_loops(pos_edge_index)
+        neg_edge_index, _ = remove_self_loops(neg_edge_index)
+        
+        neg_loss = -torch.log(1 -
+                              self.decode(z, neg_edge_index) +
+                              EPS).mean()
+
+        return pos_loss + neg_loss
+
+  def test(self,z, edge_index, y ):
+    r"""Given latent variables :obj:`z`, edges
+    computes area under the ROC curve (AUC) , average precision (AP)
+    and (Acc) accuracy scores  .
+    Args:
+        z (Tensor): The latent space :math:`\mathbf{Z}`.
+        edge_index (LongTensor):   edges
+        y  (Tensor):labels
+    """
+    pred =  self.decode(z, edge_index,)
+
+    y, pred = y.detach().cpu().numpy(), pred.detach().cpu().numpy()
+
+    return roc_auc_score(y, pred), precision_score(y, pred>=0.5), accuracy_score(y,pred>0.5)
+
+node_types = [("Y-State",11), ("Y-Transition",67), ("S-State",28)]
+
+myGAE = MyGAE(Encoder(), EdgeDecoder(64), merged_graph, node_types, node_dim=64)
+myGAE.to(device)
+train = train.to(device)
+with torch.no_grad():
+    myGAE.encode(train.x_dict, train.edge_index_dict)
+
+optimizer = torch.optim.Adam(myGAE.parameters(), lr=0.01, weight_decay=0.0005)
+
+validationMetrics = []
+for i in range(40):
+    myGAE.train()
+    optimizer.zero_grad()
+    # pred = myGAE(train.x_dict, train.edge_index_dict,
+    #              train['Gene', "associates",'Disease'].edge_label_index)
+    target = train['state', 'to', 'state'].edge_label
+    label_edge_index = train['state', 'to', 'state'].edge_label_index
+    h = myGAE.encode(train.x_dict,train.edge_index_dict)
+    loss  = myGAE.recon_loss(h,label_edge_index[:,target==1],label_edge_index[:,target==0])
+    loss.backward()
+    optimizer.step()
+    auc, p ,acc= myGAE.test(h, train['state', 'to', 'state'].edge_label_index,train['state', 'to', 'state'].edge_label)
+    # print(i,"train", np.array([auc, p , acc]).round(3),end=' ')
+    
+    with torch.no_grad():
+      myGAE.eval()
+      test.to(device)
+      z = myGAE.encode(test.x_dict,test.edge_index_dict)
+      auc, p ,acc= myGAE.test(z, test['state', 'to', 'state'].edge_label_index,test['state', 'to', 'state'].edge_label)
+      # auc, p = myGAE.test(z, valid.edge_label_index[:,valid.edge_label==1],valid.edge_label_index[:,valid.edge_label==0])
+      # print("valid",np.array([auc, p , acc]).round(3))
+      validationMetrics.append([auc, p , acc])
+
+plt.plot(np.arange(len(validationMetrics)),np.array(validationMetrics)[:,0],label='auc')
+plt.plot(np.arange(len(validationMetrics)),np.array(validationMetrics)[:,1],label='precision')
+plt.plot(np.arange(len(validationMetrics)),np.array(validationMetrics)[:,2],label='acc')
+plt.legend()
+plt.show()
